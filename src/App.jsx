e43e4ccea0dab5bb957bee9676f65c016f983bdf
tachimo <92985370+tachimo@users.noreply.github.com>
2024-01-31 15:42:20 +0000

    Add:stateについて

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const onClickCountup = () => {
+    setNum((prev) => prev + 1);
+    setNum((prev) => prev + 1);
+  };
+
+  const onClickReset = () => {
+    setNum(0);
+  };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="green">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountup}>カウントアップ</button>
+      <button onClick={onClickReset}>リセットボタン</button>
+      <p>{num}</p>
     </>
   );
 };
